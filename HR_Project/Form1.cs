fa9714e4bce512b27d8a7a3cbd04ebae8aacf41c
Musab9 <musabatieh9@gmail.com>
2024-05-18 07:35:21 +0300

    DElete DATaaccess

--- HR_Project/Form1.cs
+++ HR_Project/Form1.cs
@@ -19,109 +19,10 @@ namespace HR_Project
 
         private async void Form1_Load(object sender, EventArgs e)
         {
-           dataGridView1.DataSource= await clsUserData.GetAllUsersAsync();
         }
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
-
-        }
-        public class clsUserData
-        {
-            public static string ConnectionString = "Server=.;Database=HR_Database;User Id=sa;Password=123456;";
-
-
-
-            public async static Task<DataTable> GetAllUsersAsync()
-            {
-
-                DataTable dt = new DataTable();
-
-                using (SqlConnection connection = new SqlConnection(ConnectionString))
-                {
-
-
-                    string query = "select * from Employees_View";
-
-                    using (SqlCommand command = new SqlCommand(query, connection))
-                    {
-
-
-                        try
-                        {
-                            connection.Open();
-
-                            using (SqlDataReader reader = await command.ExecuteReaderAsync())
-                            {
-
-
-                                if (reader.HasRows)
-
-                                {
-                                    dt.Load(reader);
-                                }
-
-                            }
-
-
-                        }
-
-                        catch (Exception ex)
-                        {
-                            Console.WriteLine(ex.ToString());
-                        }
-                    }
-                }
-
-                return dt;
-
-            }
-
-            public static DataTable GetAllUsers()
-            {
-
-                DataTable dt = new DataTable();
-
-                using (SqlConnection connection = new SqlConnection(ConnectionString))
-                {
-
-
-                    string query = "select * from Employees_View";
-
-                    using (SqlCommand command = new SqlCommand(query, connection))
-                    {
-
-
-                        try
-                        {
-                            connection.Open();
-
-                            using (SqlDataReader reader = command.ExecuteReader())
-                            {
-
-
-                                if (reader.HasRows)
-
-                                {
-                                    dt.Load(reader);
-                                }
-
-                            }
-
-
-                        }
-
-                        catch (Exception ex)
-                        {
-                            Console.WriteLine(ex.ToString());
-                        }
-                    }
-                }
-
-                return dt;
-
-            }
-
 
         }
 
