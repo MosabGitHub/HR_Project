345e002436395665a71c3dc69aee9dfaafb253e4
Musab9 <musabatieh9@gmail.com>
2024-05-18 07:03:32 +0300

    New changes

--- HR_Project/Form1.cs
+++ HR_Project/Form1.cs
@@ -2,13 +2,12 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using AAdlBusiness;
-using DataAccessLib;
 namespace HR_Project
 {
     public partial class Form1 : Form
@@ -18,9 +17,113 @@ namespace HR_Project
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private async void Form1_Load(object sender, EventArgs e)
         {
-           dataGridView1.DataSource= clsUserData.GetAllUsers();
+           dataGridView1.DataSource= await clsUserData.GetAllUsersAsync();
         }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+        public class clsUserData
+        {
+            public static string ConnectionString = "Server=.;Database=HR_Database;User Id=sa;Password=123456;";
+
+
+
+            public async static Task<DataTable> GetAllUsersAsync()
+            {
+
+                DataTable dt = new DataTable();
+
+                using (SqlConnection connection = new SqlConnection(ConnectionString))
+                {
+
+
+                    string query = "select * from Employees_View";
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+
+
+                        try
+                        {
+                            connection.Open();
+
+                            using (SqlDataReader reader = await command.ExecuteReaderAsync())
+                            {
+
+
+                                if (reader.HasRows)
+
+                                {
+                                    dt.Load(reader);
+                                }
+
+                            }
+
+
+                        }
+
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.ToString());
+                        }
+                    }
+                }
+
+                return dt;
+
+            }
+
+            public static DataTable GetAllUsers()
+            {
+
+                DataTable dt = new DataTable();
+
+                using (SqlConnection connection = new SqlConnection(ConnectionString))
+                {
+
+
+                    string query = "select * from Employees_View";
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+
+
+                        try
+                        {
+                            connection.Open();
+
+                            using (SqlDataReader reader = command.ExecuteReader())
+                            {
+
+
+                                if (reader.HasRows)
+
+                                {
+                                    dt.Load(reader);
+                                }
+
+                            }
+
+
+                        }
+
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.ToString());
+                        }
+                    }
+                }
+
+                return dt;
+
+            }
+
+
+        }
+
     }
 }
